50d4d42f11d8fa0797edc3a59543264971631c85
Ildar Mindiyarov <77970650+AnMSLbR@users.noreply.github.com>
2023-04-01 04:48:55 +0500

    Feature-2. Downloading images from the URLs list to the zip.

--- URLDownloader/MainWindow.Designer.cs
+++ URLDownloader/MainWindow.Designer.cs
@@ -33,24 +33,30 @@
             fbd_OutputPath = new FolderBrowserDialog();
             tb_OutputPath = new TextBox();
             btn_OutputPath = new Button();
+            ofd_UrlList = new OpenFileDialog();
+            tb_UrlList = new TextBox();
+            btn_UrlList = new Button();
+            btn_SaveAll = new Button();
+            lbl_CounterLabel = new Label();
+            lbl_FileCounter = new Label();
             SuspendLayout();
             // 
             // tb_URL
             // 
-            tb_URL.Location = new Point(12, 66);
+            tb_URL.Location = new Point(12, 82);
             tb_URL.Name = "tb_URL";
             tb_URL.Size = new Size(294, 23);
             tb_URL.TabIndex = 0;
             // 
             // btn_Save
             // 
-            btn_Save.Location = new Point(231, 95);
+            btn_Save.Location = new Point(231, 111);
             btn_Save.Name = "btn_Save";
             btn_Save.Size = new Size(75, 23);
             btn_Save.TabIndex = 1;
-            btn_Save.Text = "Скачать";
+            btn_Save.Text = "Download";
             btn_Save.UseVisualStyleBackColor = true;
-            btn_Save.Click += btn_Save_Click;
+            btn_Save.Click += btn_Save_ClickAsync;
             // 
             // tb_OutputPath
             // 
@@ -69,11 +75,65 @@
             btn_OutputPath.UseVisualStyleBackColor = true;
             btn_OutputPath.Click += btn_OutputPath_Click;
             // 
+            // ofd_UrlList
+            // 
+            ofd_UrlList.FileName = "openFileDialog1";
+            ofd_UrlList.Filter = "Текстовые файлы(*.txt)|*.txt";
+            // 
+            // tb_UrlList
+            // 
+            tb_UrlList.Location = new Point(12, 152);
+            tb_UrlList.Name = "tb_UrlList";
+            tb_UrlList.Size = new Size(212, 23);
+            tb_UrlList.TabIndex = 4;
+            // 
+            // btn_UrlList
+            // 
+            btn_UrlList.Location = new Point(231, 153);
+            btn_UrlList.Name = "btn_UrlList";
+            btn_UrlList.Size = new Size(75, 23);
+            btn_UrlList.TabIndex = 5;
+            btn_UrlList.Text = "...";
+            btn_UrlList.UseVisualStyleBackColor = true;
+            btn_UrlList.Click += btn_UrlList_Click;
+            // 
+            // btn_SaveAll
+            // 
+            btn_SaveAll.Location = new Point(231, 182);
+            btn_SaveAll.Name = "btn_SaveAll";
+            btn_SaveAll.Size = new Size(75, 23);
+            btn_SaveAll.TabIndex = 6;
+            btn_SaveAll.Text = "Download";
+            btn_SaveAll.UseVisualStyleBackColor = true;
+            btn_SaveAll.Click += btn_SaveAll_Click;
+            // 
+            // lbl_CounterLabel
+            // 
+            lbl_CounterLabel.AutoSize = true;
+            lbl_CounterLabel.Location = new Point(12, 387);
+            lbl_CounterLabel.Name = "lbl_CounterLabel";
+            lbl_CounterLabel.Size = new Size(72, 15);
+            lbl_CounterLabel.TabIndex = 7;
+            lbl_CounterLabel.Text = "Files loaded:";
+            // 
+            // lbl_FileCounter
+            // 
+            lbl_FileCounter.AutoSize = true;
+            lbl_FileCounter.Location = new Point(90, 387);
+            lbl_FileCounter.Name = "lbl_FileCounter";
+            lbl_FileCounter.Size = new Size(0, 15);
+            lbl_FileCounter.TabIndex = 8;
+            // 
             // MainWindow
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(334, 411);
+            Controls.Add(lbl_FileCounter);
+            Controls.Add(lbl_CounterLabel);
+            Controls.Add(btn_SaveAll);
+            Controls.Add(btn_UrlList);
+            Controls.Add(tb_UrlList);
             Controls.Add(btn_OutputPath);
             Controls.Add(tb_OutputPath);
             Controls.Add(btn_Save);
@@ -95,5 +155,11 @@
         private FolderBrowserDialog fbd_OutputPath;
         private TextBox tb_OutputPath;
         private Button btn_OutputPath;
+        private OpenFileDialog ofd_UrlList;
+        private TextBox tb_UrlList;
+        private Button btn_UrlList;
+        private Button btn_SaveAll;
+        private Label lbl_CounterLabel;
+        private Label lbl_FileCounter;
     }
 }
--- URLDownloader/MainWindow.cs
+++ URLDownloader/MainWindow.cs
@@ -2,15 +2,18 @@ using System;
 using System.Net;
 using System.Security.Policy;
 using System.IO;
+using System.IO.Compression;
 
 namespace URLDownloader
 {
     public partial class MainWindow : Form
     {
-        string _outputPath = Directory.GetCurrentDirectory();
+        string _outputPath = "";
         string _url = "";
+        string _urlListPath = "";
+        string _zipName = "";
         HttpClient _httpClient;
-        
+
         public MainWindow()
         {
             InitializeComponent();
@@ -19,24 +22,67 @@ namespace URLDownloader
         private void MainWindow_Load(object sender, EventArgs e)
         {
             _httpClient = new HttpClient();
+            _outputPath = Directory.GetCurrentDirectory();
             tb_OutputPath.Text = _outputPath;
+            _zipName = "images.zip";
         }
 
         private void btn_OutputPath_Click(object sender, EventArgs e)
         {
             if (fbd_OutputPath.ShowDialog() == DialogResult.OK)
-            {
                 tb_OutputPath.Text = fbd_OutputPath.SelectedPath;
-            }
         }
 
-        private async void btn_Save_Click(object sender, EventArgs e)
+        private void btn_Save_ClickAsync(object sender, EventArgs e)
         {
             _outputPath = tb_OutputPath.Text;
             _url = tb_URL.Text;
-            var responseStream = await _httpClient.GetStreamAsync(_url);
-            using var fileStream = new FileStream(_outputPath + _url.Substring(_url.LastIndexOf('/')), FileMode.Create);
+            DownloadImageAsync(_url);
+        }
+
+        private void btn_UrlList_Click(object sender, EventArgs e)
+        {
+            if (ofd_UrlList.ShowDialog() == DialogResult.OK)
+                tb_UrlList.Text = ofd_UrlList.FileName;
+        }
+
+        private async void btn_SaveAll_Click(object sender, EventArgs e)
+        {
+            _outputPath = tb_OutputPath.Text;
+            _urlListPath = tb_UrlList.Text;
+            var zipPath = _outputPath + "\\" + _zipName;
+            var counter = 0;
+
+            using StreamReader reader = new StreamReader(_urlListPath);
+            while ((_url = reader.ReadLine()) != null)
+            {
+                await DownloadToZipImagesAsync(_url, zipPath);
+                counter++;
+                lbl_FileCounter.Text = counter.ToString();
+            }
+
+            MessageBox.Show("Completed");
+        }
+
+        private async void DownloadImageAsync(string url)
+        {
+            using var responseStream = await _httpClient.GetStreamAsync(url);
+            var fileName = url.Substring(url.LastIndexOf('/') + 1);
+            using var fileStream = new FileStream((_outputPath + "\\" + fileName), FileMode.Create);
             responseStream.CopyTo(fileStream);
         }
+
+        private async Task DownloadToZipImagesAsync(string url, string zipPath)
+        {
+            var fileName = url.Substring(url.LastIndexOf('/') + 1);
+
+            using var responseStream = await _httpClient.GetStreamAsync(url);
+            using var zipStream = new FileStream(zipPath, FileMode.OpenOrCreate);
+            using var zip = new ZipArchive(zipStream, ZipArchiveMode.Update);
+            var file = zip.CreateEntry(fileName);
+            using var fileStream = file.Open();
+            responseStream.CopyTo(fileStream);
+        }
+
     }
 }
